0675e9eccd5d4d972bf67f6ea19eb7fd2076b696
kallmeehkev <kevin.la79@gmail.com>
2019-09-29 20:44:20 -0700

    remove comments from gallery index

--- frontend/components/gallery/gallery_index.jsx
+++ frontend/components/gallery/gallery_index.jsx
@@ -19,20 +19,11 @@ class GalleryIndex extends React.Component {
     }
 
     componentDidMount() {
-        if (!this.props.currentProfileId) {
-            this.props.openModal('profile');
-        }
+        if (!this.props.currentProfileId) { this.props.openModal('profile'); }
         this.props.fetchMedium(14);
         document.addEventListener('scroll', _.throttle(this.checkIfNeedsMoreContent, 300), { passive: true });
         let genresArr = Object.values(this.props.genres);
         let genresArrlimit3 = genresArr.filter((genre, i) => i < 3)
-        // let that = this;
-        // let showRowsFirstTwo = genresArrlimit2.map((genre, i) => {
-        //     let numCurrentlyFetched = that.state.genresFetched;
-        //     let rowActive = (that.state.open && (i === that.state.activeRowIdx));
-        //     return <GalleryShowRowContainer genre={genre} key={genre.id} pageType="genre" rowActive={rowActive} i={i} open={that.state.open} activeRowIdx={that.state.activeRowIdx}
-        //         handleActiveRow={() => this.handleActiveRow(numCurrentlyFetched + i)} /> 
-        // })
         this.setState({genreRows: genresArrlimit3, genresFetched: 3});
     }
 
@@ -51,11 +42,6 @@ class GalleryIndex extends React.Component {
                  return (i >= bottomLimit && i < bottomLimit + 3)  
             })
             let threeMoreRows = genresArrlimit3;
-            // let twoMoreRows = genresArrlimit2.map((genre, i) => {
-            //     let rowActive = (this.state.open && (this.state.genresFetched + i === this.state.activeRowIdx));
-            //     return <GalleryShowRowContainer genre={genre} key={genre.id} pageType="genre" rowActive={rowActive}
-            //         handleActiveRow={() => this.handleActiveRow(this.state.genresFetched + i)} />
-            // })
             let currentRows = this.state.genreRows;
             let currentlyFetched = this.state.genresFetched;
             this.setState({ genreRows: currentRows.concat(threeMoreRows), genresFetched: currentlyFetched + 3 });
@@ -68,13 +54,6 @@ class GalleryIndex extends React.Component {
 
     render() {
         if (this.props.genres[12] && this.props.mediaGenres[140]) {
-            // let genresArr = Object.values(this.props.genres);
-            // let genresArrlimit6 = genresArr.filter( (genre, i) => i < 6) 
-            // let showRowsFirstSix = genresArrlimit6.map( (genre, i) => {
-            //     let rowActive = (this.state.open && (i === this.state.activeRowIdx));
-            //     return <GalleryShowRowContainer genre={genre} key={genre.id} pageType="genre" rowActive={rowActive}
-            //     handleActiveRow={()=>this.handleActiveRow(i)}/> 
-            // })
             let genreRows = this.state.genreRows.map( (genre, i) => {
                 let rowActive = (this.state.open && (i === this.state.activeRowIdx));
                 return <GalleryShowRowContainer genre={genre} key={i} pageType="genre" rowActive={rowActive}
@@ -82,9 +61,7 @@ class GalleryIndex extends React.Component {
             })
             return (
                 <div className="browse_body">
-                    {/* <GalleryNavBarContainer path={this.props.match.path}/> */}
                     <GalleryFPVideoContainer genreId={5} pageType="browse" browseVid={this.props.fpVideo} />
-                    {/* {showRowsFirstSix} */}
                     {genreRows}
                 </div>
             )
